1ce5b41e66eba9e873cb9964f668c1df24008463
daniboygg <danigayosog@gmail.com>
2025-10-05 01:01:27 +0200

    Adjust screen mode and speed

--- memory.gd
+++ memory.gd
@@ -3,14 +3,14 @@ class_name Memory extends Node2D
 
 @export var height := 10
 @export var width := 40
-@export var speed := 20.0
+@export var speed := 125.0
 @export var deviation := speed * 0.2
 
 @onready var color_rect: ColorRect = $ColorRect
 @onready var area_2d: Area2D = $Area2D
 @onready var collision_shape_2d: CollisionShape2D = $Area2D/CollisionShape2D
 
-@onready var label: Label = $Label
+
 
 func _ready():
 	speed = randf_range(speed - deviation, speed + deviation)
